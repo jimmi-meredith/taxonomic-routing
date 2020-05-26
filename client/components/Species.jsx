7ed20b8e16eba750990527c4f5d00cf69f1809fd
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-27 10:19:20 +1200

    Build Species component

--- client/components/Species.jsx
+++ client/components/Species.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Species = () => {
+import species from '../../data/species'
 
+const Species = ({ match }) => {
+  const { speciesId } = match.params
+  const speciesDetails = species.filter(item => item.id === speciesId)[0]
+  return (
+    <div className='species'>
+      <h2>{speciesDetails.name}</h2>
+      <div>
+        <p>{speciesDetails.description}</p>
+        <img src={speciesDetails.photo}/>
+      </div>
+      <ul>
+        <li>Kingdom: <Link to={`/rank/kingdoms/${speciesDetails.kingdom}`}>{speciesDetails.kingdom}</Link></li>
+        <li>Phylum: <Link to={`/rank/phyla/${speciesDetails.phylum}`}>{speciesDetails.phylum}</Link></li>
+        <li>Class: <Link to={`/rank/classes/${speciesDetails.class}`}>{speciesDetails.class}</Link></li>
+        <li>Order: <Link to={`/rank/orders/${speciesDetails.order}`}>{speciesDetails.order}</Link></li>
+        <li>Family: <Link to={`/rank/families/${speciesDetails.family}`}>{speciesDetails.family}</Link></li>
+        <li>Genus: <Link to={`/rank/genera/${speciesDetails.genus}`}>{speciesDetails.genus}</Link></li>
+      </ul>
+    </div>
+  )
 }
 
 export default Species
