900ba779e507eec5cbc745d0be0aa4bfdb06aa27
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-26 16:20:53 +1200

    Fix error in filter function

--- client/components/RankItem.jsx
+++ client/components/RankItem.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import ranks from '../../data/ranks'
+
 const RankItem = ({ match }) => {
-  const 
+  const { rankName, rankItem } = match.params
+  const rankDetails = ranks[rankName].filter(item => item.name === rankItem)[0]
+
+  return (
+    <div className='rank-list'>
+      <h2>{rankName}</h2>
+      <p>{rankDetails.description}</p>
+    </div>
+  )
 }
 
 export default RankItem
