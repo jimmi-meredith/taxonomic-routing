53b06e6f105e071857c6409b9889a26b94087235
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-26 15:35:32 +1200

    Build RankList component

--- client/components/RankList.jsx
+++ client/components/RankList.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const RankList = () => {
+import ranks from '../../data/ranks'
 
+const RankList = ({ match }) => {
+  const { rankName } = match.params
+  const rankList = ranks[rankName]
+  return (
+    <div className='rank-list'>
+      <h2>{rankName}</h2>
+      <ul>
+        {rankList.map(rankItem => {
+          return <li><Link to={`/rank/${rankName}/${rankItem.name}`}>{rankItem.name}</Link></li>
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default RankList
